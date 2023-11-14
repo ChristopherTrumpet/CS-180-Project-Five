847f88410c5dcfa6c8f741df1e47d772add3ed5a
ChristopherTrumpet <ctrumpet@purdue.edu>
2023-11-14 01:04:14 -0500

    Added getter and setters for user file

--- Marketplace/Server-Side/src/backend/usertypes/User.java
+++ Marketplace/Server-Side/src/backend/usertypes/User.java
@@ -5,4 +5,31 @@ public class User {
     private String password;
     private String fileDirectory;
 
+    // GETTER METHODS
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFileDirectory() {
+        return fileDirectory;
+    }
+
+    // SETTER METHODS
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFileDirectory(String fileDirectory) {
+        this.fileDirectory = fileDirectory;
+    }
 }
