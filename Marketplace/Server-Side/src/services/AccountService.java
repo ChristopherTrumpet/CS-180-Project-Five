8453e144af2e3db24efbe82501f803b048d94a3e
ChristopherTrumpet <ctrumpet@purdue.edu>
2023-11-30 07:02:10 -0500

    Added documentation and minor changes

--- Marketplace/Server-Side/src/services/AccountService.java
+++ Marketplace/Server-Side/src/services/AccountService.java
@@ -11,16 +11,44 @@ import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Random;
 
+/**
+ * Marketplace Application : Account Service
+ *
+ * <p>
+ * This service handles each users account details as well as account creation / deletion
+ * </p>
+ *
+ * @author [NAMES], CS 180 Lab
+ * @version v0.0.1
+ */
+
 public class AccountService {
 
+    // Directory of the users.json file which stores all user data
     public final String userFileDirectory;
 
+    /**
+     * Constructor initializes user file directory, creates a new one if one does not exist.
+     */
     public AccountService() {
+
+        // TODO: Check if file exists, create a new one if it does not
+
         this.userFileDirectory = Paths.get(System.getProperty("user.dir") + "\\data\\users.json").toString();
     }
 
+    /**
+     * Creates a new user object in the users.json, populated by the parameters.
+     *
+     * @param accountType Indicates whether user is a buyer or seller
+     * @param userName    Stores user's username
+     * @param password    Stores user's password, may encrypt later
+     * @param email       Stores user's email
+     * @param firstName   Stores user's first name
+     * @param lastName    Stores user's last name
+     * @return True if account was successfully created, false otherwise
+     */
     public boolean createAccount(
-
             char accountType,
             String userName,
             String password,
@@ -28,13 +56,17 @@ public class AccountService {
             String firstName,
             String lastName) {
 
+        // Creates a new user id
         String userId = generateUserId(accountType);
+
         JSONObject userObj = new JSONObject();
 
+        // Ensures users.json file exists
         if (getUserFile() != null)
             userObj = new JSONObject(getUserFile());
 
-        if (userExists(email, userObj))
+        // Uses user email as it is unique, user id cannot be used as it generated upon creation
+        if (userExists(email))
             return false;
 
         JSONArray users = userObj.getJSONArray("users");
@@ -47,31 +79,48 @@ public class AccountService {
         user.put("first_name", firstName);
         user.put("last_name", lastName);
 
+        // Create an empty cart if the user is a buyer
         if (accountType == 'b') {
             user.put("cart", new JSONArray());
         }
 
         users.put(user);
-
         userObj.put("users", users);
 
         return writeUserToFile(userObj);
 
     }
 
-    private boolean userExists(String email, JSONObject userObj) {
+    /**
+     * Checks if user exists in users.json database by email
+     *
+     * @param email The email of the sought out user
+     * @return True if user exists, false otherwise
+     */
+    private boolean userExists(String email) {
         return getUserByEmail(email) != null;
     }
 
+    /**
+     * Checks if the user is a buyer
+     *
+     * @param userId Gets user from database using their unique user id
+     * @return True if the user is a buyer, false otherwise
+     */
     public boolean isBuyer(String userId) {
-        JSONObject user = getUserById(userId);
         return userId.charAt(userId.length() - 1) == 'b';
     }
 
+    /**
+     * Checks if the user is a seller
+     *
+     * @param userId Gets user from database using their unique user id
+     * @return True if the user is a seller, false otherwise
+     */
     public boolean isSeller(String userId) {
-        JSONObject user = getUserById(userId);
         return userId.charAt(userId.length() - 1) == 's';
     }
+
 
     public String getUserFileDirectory() {
         return userFileDirectory;
@@ -138,13 +187,14 @@ public class AccountService {
     }
 
     public boolean writeUserToFile(JSONObject userObj) {
+
         try {
+
             FileWriter file = new FileWriter(getUserFileDirectory());
             file.write(userObj.toString());
             file.flush();
             file.close();
             return true;
-
 
         } catch (IOException e) {
             System.out.println("Error occurred writing json object to file...\n" + e.getMessage());
@@ -153,12 +203,10 @@ public class AccountService {
         return false;
     }
 
-    public String[] getAccountDetails(String userId) throws IOException {
+    public String[] getAccountDetails(String userId) {
 
-        String userFile = Files.readString(Paths.get("E:\\dev\\CS-180-Project-Five\\Marketplace\\Server-Side\\data\\users.json"));
         String[] accountDetails = new String[6];
 
-        JSONObject obj = new JSONObject(userFile);
         JSONObject user = getUserById(userId);
 
         if (user == null)
@@ -169,19 +217,27 @@ public class AccountService {
         accountDetails[2] = user.get("email").toString();
         accountDetails[3] = user.get("first_name").toString();
         accountDetails[4] = user.get("last_name").toString();
-        accountDetails[5] = userId.charAt(userId.length()-1) == 'b' ? "Buyer" : "Seller";
+        accountDetails[5] = userId.charAt(userId.length() - 1) == 'b' ? "Buyer" : "Seller";
 
         return accountDetails;
     }
 
+    /**
+     * Generates a unique user id string
+     * @param AccountType
+     * @return
+     */
     public String generateUserId(char AccountType) {
-        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        StringBuilder salt = new StringBuilder();
+
+        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; // Potential characters for id
+        StringBuilder userId = new StringBuilder();
         Random rnd = new Random();
-        while (salt.length() < 18) { // length of the random string.
+
+        while (userId.length() < 16) { // Creates a user id 16 characters long
             int index = (int) (rnd.nextFloat() * chars.length());
-            salt.append(chars.charAt(index));
+            userId.append(chars.charAt(index));
         }
-        return salt.toString()+AccountType;
+
+        return userId.toString() + AccountType;
     }
 }
